a3ab9eb43569de1a4a41ce63e091ea8d42cebb54
Nick Mitselos <nick.mitselos@gmail.com>
2020-04-23 18:20:37 +0300

    Adding Actions

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
+
 class Auth extends Component {
     state = {
         controls: {
@@ -71,7 +74,7 @@ class Auth extends Component {
     inputChangedHandler = (event, controlName) => {
         const updatedControls = {
             ...this.state.controls,
-            [controlName]: {
+            [controlName]: {//override the arguments
                 ...this.state.controls[controlName],
                 value: event.target.value,
                 valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
@@ -80,6 +83,11 @@ class Auth extends Component {
         };
         this.setState({controls: updatedControls});
 
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value)
     }
 
     render() {
@@ -103,7 +111,7 @@ class Auth extends Component {
         ))
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">Submit</Button>
                 </form>
@@ -112,4 +120,10 @@ class Auth extends Component {
     }
  
 }
-export default Auth;
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
+export default connect(null, mapDispatchToProps)(Auth) ;
