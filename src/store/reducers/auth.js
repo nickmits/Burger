dcb7ee0957b0750e8869c0282cc03b728204bc19
Nick Mitselos <nick.mitselos@gmail.com>
2020-04-29 18:05:01 +0300

    Using env variables/add lazy loading

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from './utility';
+import { updateObject } from '../../shared/utility';
 import { auth } from '../actions';
 
 const initialState = {
